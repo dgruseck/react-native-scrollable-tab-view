3a2cda73fd0049fbe59fc99a28417fa1e75e74c2
dgruseck <david@gruseck.eu>
2017-08-28 19:23:53 +0200

    Update ScrollableTabBar.js

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const PropTypes require('prop-types');
+const PropTypes = require('prop-types');
 const { ViewPropTypes } = ReactNative = require('react-native');
 const {
   View,
